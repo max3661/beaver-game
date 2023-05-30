60fc86006ed5fcd81ae497bb4eee0cc56c5444d4
max3661 <m.wagner@student.xu-university.de>
2023-05-30 15:04:32 +0200

    Update ThirdPersonCam.cs

--- BeaverGame/Assets/Scripts/ThirdPersonCam.cs
+++ BeaverGame/Assets/Scripts/ThirdPersonCam.cs
@@ -30,7 +30,7 @@ public class ThirdPersonCam : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
+        
         if (inputDir != Vector3.zero)
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
 
