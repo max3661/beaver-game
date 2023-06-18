5fa9605651ad4959dd435fa6baa8436a3df12002
Miri <miri.moehre@web.de>
2023-06-18 23:22:25 +0200

    try to connect swimming animation

--- BeaverGame/Assets/Scripts/AnimationStateController.cs
+++ BeaverGame/Assets/Scripts/AnimationStateController.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     Animator animator;
+    public bool isSwimming;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        isSwimming = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         bool isWalking = animator.GetBool("isWalking");
-        bool isSwimming = animator.GetBool("isSwimming");
+        bool swimCheck = animator.GetBool("swimCheck");
         bool pressedMovementKey = Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d") || Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right");
 
         //if player presses w key
@@ -35,11 +37,11 @@ public class AnimationStateController : MonoBehaviour
         }
 
         //swimming
-        if (!isSwimming) {
-            animator.SetBool("isSwimming", true);
+        if (!swimCheck && isSwimming) {
+            animator.SetBool("swimCheck", true);
         }
-        if (isSwimming) {
-            animator.SetBool("isSwimming", false);
+        else if (swimCheck && !isSwimming) {
+            animator.SetBool("swimCheck", false);
         }
 
     }
